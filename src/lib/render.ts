f5743b50740e0c0c721c80ac945ebfe8fbd3e1a3
lloydzhou <lloydzhou@qq.com>
2023-02-28 07:58:06 +0800

    update

--- src/lib/render.ts
+++ src/lib/render.ts
@@ -8,23 +8,16 @@ export function ElementOf<Element, T extends string>(type: T, Ctor: () => any): 
   return type as any
 }
 
-const patchProp = function(el, key, prevValue, nextValue, isSVG, prevChildren, parentComponent, parentSuspense, unmountChildren) {
-  el._update(key, prevValue, nextValue)
-}
-
-const createElement = function(type, isSVG, isCustomizedBuiltIn, vnodeProps){
-  const Ctor = componentsMap.get(type)
-  const com = Ctor && Ctor(vnodeProps)
-  return com
-}
-
 // https://vuejs.org/api/custom-renderer.html#createrenderer
 const { render, createApp } = createRenderer({
-  patchProp,
+  patchProp: (el, key, prevValue, nextValue) => el._update(key, prevValue, nextValue),
   insert: (el, parent) => el && el._insert(parent),
   remove: (el) => el && el._remove(),
-  createElement,
-  createText(text) {
+  createElement: (type, isSVG, isCustomizedBuiltIn, vnodeProps) => {
+    const Ctor = componentsMap.get(type)
+    return Ctor && Ctor(vnodeProps)
+  },
+  createText: (text) => {
     const t = {text, _insert: (parent) => t.parentNode = parent}
     return t
   },
