454a39dc6e156d0112a9cd9ae2598eda9a9345ff
lloydzhou <lloydzhou@qq.com>
2023-02-28 08:08:09 +0800

    bindevent to graph

--- src/lib/Graph.tsx
+++ src/lib/Graph.tsx
@@ -4,6 +4,34 @@ import { defineComponent, onMounted, onUnmounted, ref, shallowReactive, h, nextT
 import { ElementOf, render } from './render'
 import type { DefineComponent, Ref } from 'vue'
 
+const processProps = (props) => {
+  return Object.entries(props).reduce((res, [name, value]) => {
+    if (name.startsWith('on')) {
+      res.events[name.substr(2).toLowerCase()] = value
+    } else {
+      // enabled --> enabled: true
+      res.props[name] = value === "" ? true : value
+    }
+    return res
+  }, {props:{}, events: {}})
+}
+
+const bindEvent = (node, events, graph) => {
+  // 绑定事件都是包了一层的，返回一个取消绑定的函数
+  const ubindEvents = Object.entries(events).map(([name, callback]) => {
+    const handler = (e) => {
+      const { cell } = e
+      if (node && cell.id === node.id) {
+        // @ts-ignore
+        callback(e)
+      }
+    }
+    graph.on(`cell:${name}`, handler)
+    return () => graph.off(`cell:${name}`, handler)
+  })
+  return () => ubindEvents.forEach(h => h())
+}
+
 interface Props {
   container?: Ref<HTMLElement>;
 }
@@ -14,7 +42,8 @@ export const Graph = defineComponent({
   name: 'Graph',
   setup(props, { attrs, slots, expose }) {
     const { container } = props
-    const { width=800, height=600, ...other } = attrs
+    const { props: gprops={}, events={} } = processProps(attrs)
+    const { width=800, height=600, ...other } = gprops
     const containerRef = ref<HTMLElement | undefined>(container)
     const context = shallowReactive<{graph: X6Graph | null}>({ graph: null })
     const children = ref()
@@ -22,7 +51,6 @@ export const Graph = defineComponent({
     expose(context)
     onMounted(() => {
       // options
-      Object.keys(other).forEach(key => other[key] === "" && (other[key] = true) )
       if (containerRef.value) {
         context.graph = new X6Graph({
           container: containerRef.value,
@@ -30,6 +58,7 @@ export const Graph = defineComponent({
           height,
           ...other,
         })
+        bindEvent(null, events, context.graph)
 
         const item = h(() => (children.value || []).filter(i => i.props).map(i => {
           // 将key重置，不更改props这些信息
@@ -64,33 +93,6 @@ export const Graph = defineComponent({
     }
   }
 }) as Graph
-
-const processProps = (props) => {
-  return Object.entries(props).reduce((res, [name, value]) => {
-    if (name.startsWith('on')) {
-      res.events[name.substr(2).toLowerCase()] = value
-    } else {
-      res.props[name] = value
-    }
-    return res
-  }, {props:{}, events: {}})
-}
-
-const bindEvent = (node, events, graph) => {
-  // 绑定事件都是包了一层的，返回一个取消绑定的函数
-  const ubindEvents = Object.entries(events).map(([name, callback]) => {
-    const handler = (e) => {
-      const { cell } = e
-      if (node && cell.id === node.id) {
-        // @ts-ignore
-        callback(e)
-      }
-    }
-    graph.on(`cell:${name}`, handler)
-    return () => graph.off(`cell:${name}`, handler)
-  })
-  return () => ubindEvents.forEach(h => h())
-}
 
 const createCell = (Ctor, shape, newProps) => {
   const { props={}, events={} } = processProps(newProps)
